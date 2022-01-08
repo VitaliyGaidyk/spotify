7b94d877fff4e41faf580faddef28b1553f87a03
VitaliyGaidyk <ishimura20@gmail.com>
2022-01-08 18:00:01 +0200

    fix

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,12 @@
 import {getProviders, signIn} from "next-auth/react";
 
 const Login = ({providers}) => {
-
     return (
         <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
             <img className="w-52 mb-5" src="https://i.imgur.com/fPuEa9V.png" alt=""/>
             {Object.values(providers).map((provider) => (
                 <div key={provider.name}>
-                    <button onClick={() => signIn(provider.id, {callbackUrl: false})}
+                    <button onClick={() => signIn(provider.id, {callbackUrl: "/"})}
                             className="bg-[#18D860] text-white p-5 rounded-lg">
                         Login with {provider.name}
                     </button>
