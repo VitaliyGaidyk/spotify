f9a74469ec7d662a5f5babb12d913659a87f2bbd
VitaliyGaidyk <ishimura20@gmail.com>
2022-01-11 23:14:41 +0200

    add HOC

---
+++ Layouts/Layout.js
@@ -0,0 +1,19 @@
+import Sidebar from "../components/Sidebar";
+import Center from "../components/Center";
+import Player from "../components/Player";
+
+ const Layout = ({children}) => {
+     return (
+            <div className="bg-black h-screen overflow-hidden">
+                <main className='flex'>
+                    <Sidebar/>
+                    {children}
+                </main>
+
+                <div className='sticky bottom-0'>
+                    <Player />
+                </div>
+            </div>
+    );
+};
+export default Layout
---
+++ pages/library.js
@@ -0,0 +1,9 @@
+const Library = () => {
+    return (
+       <div className="bg-black h-screen overflow-hidden">
+           LIBRARY
+       </div>
+    );
+};
+
+export default Library;
