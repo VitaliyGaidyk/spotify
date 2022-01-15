2f458f09ccd9b1277c4b190fcb66fde71d6811fa
VitaliyGaidyk <ishimura20@gmail.com>
2022-01-15 18:52:46 +0200

    404-style

--- pages/404.js
+++ pages/404.js
@@ -3,26 +3,29 @@ import SidebarButton from "../components/SidebarButton";
 
 const ErrorPage = () => {
     return (
-        <>
+        <section className='bg-black h-screen overflow-hidden text-white'>
             <head>
                 <title>Page not found</title>
             </head>
-            <div>
+            <div className='flex flex-col items-center min-h-screen w-full justify-center '>
                 <SidebarButton id='404'/>
-                <h1>Page not found</h1>
-                <p>We can’t seem to find the page you are looking for.</p>
-            </div>
-            <div>
+                <h1 className='text-5xl	font-bold mt-8 mb-5'>Page not found</h1>
+                <p className='mb-4'>We can’t seem to find the page you are looking for.</p>
                 <Link href='/'>
-                    <button >
+                    <button className='padding-button mb-8 rounded-full bg-white text-black text-lg font-bold'>
                         Home
                     </button>
                 </Link>
                 <Link href="https://support.spotify.com/us/">
-                    Help
+                    <button className='font-bold'>
+                        Help
+                    </button>
                 </Link>
             </div>
-        </>
+            <div>
+
+            </div>
+        </section>
     );
 };
 
