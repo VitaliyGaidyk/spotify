d473f44e11ddcdcc42d9006efa48a2cc57ea6fd2
VitaliyGaidyk <ishimura20@gmail.com>
2022-01-22 17:42:26 +0200

    add songs library

--- components/Songs.js
+++ components/Songs.js
@@ -8,7 +8,7 @@ const Songs = () => {
     return (
         <div className='px-8 flex flex-col space-y-1 pb-28 text-white'>
             {playlist?.tracks.items.map((track, index) => (
-                <Song key={track.track.id} track={track} order={index}/>
+                <Song key={index} track={track} order={index}/>
                 ))}
         </div>
     );
--- pages/library.js
+++ pages/library.js
@@ -1,16 +1,28 @@
 import Layout from "../layouts/Layout";
 import usePlaylist from "../hooks/usePlaylist";
-import {useRecoilState} from "recoil";
-import {playlistIdState} from "../atoms/playlistAtom";
+import {useRecoilState, useRecoilValue} from "recoil";
+import {playlistIdState, playlistState} from "../atoms/playlistAtom";
+import {useEffect} from "react";
+import useSpotify from "../hooks/useSpotify";
+import Songs from "../components/Songs";
 
 const Library = () => {
+    const spotifyApi = useSpotify()
     const [playlistsId, setPlaylistsId] = useRecoilState(playlistIdState)
+    const [playlist, setPlaylist] = useRecoilState(playlistState)
     const playlists = usePlaylist()
+
+    useEffect(() => {
+        spotifyApi.getPlaylist(playlistsId)
+            .then((data) => {
+                setPlaylist(data.body)
+            })
+            .catch(err => console.log('some error', err))
+    }, [spotifyApi, playlistsId])
 
     return (
         <Layout title='Library'>
             <div className={'flex-grow h-screen overflow-y-scroll scrollbar-hide'}>
-
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-7 gap-4 p-2'>
                     {playlists.map((playlist) => (
                         <div className="p-3 cursor-pointer bg-[#181818] md:text-sm
@@ -30,7 +42,7 @@ const Library = () => {
                         </div>
                     ))}
                 </div>
-
+                <Songs/>
             </div>
         </Layout>
     );
